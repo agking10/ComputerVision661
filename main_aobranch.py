e73f6c65343e681360a12d1b42e50425674c048f
aoom12 <32113669+aoom12@users.noreply.github.com>
2020-12-03 13:04:11 -0500

    Add files via upload
    Track both red and green balls.

--- main_aobranch.py
+++ main_aobranch.py
@@ -12,13 +12,20 @@ from play import play
 import threading
 
 
-def detect_objects(img, pts, num_objs = 1):
+def detect_objects(img, pts, num_objs = 1, color = 'green'):
     blurred = cv2.GaussianBlur(img, (11, 11), 0)
-    hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
+    #todo change back to HSV??
+    hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2RGB)
     # construct a mask for the color "green", then perform
     # a series of dilations and erosions to remove any small
     # blobs left in the mask
-    mask = cv2.inRange(hsv, greenLower, greenUpper)
+    if (color == 'green'):
+        upper_hsv = greenUpper
+        lower_hsv = greenLower
+    if (color == 'red'):
+        upper_hsv = redUpper
+        lower_hsv = redLower
+    mask = cv2.inRange(hsv, lower_hsv, upper_hsv)
     mask = cv2.erode(mask, None, iterations=2)
     mask = cv2.dilate(mask, None, iterations=2)
     # find contours in the mask and initialize the current
@@ -35,16 +42,8 @@ def detect_objects(img, pts, num_objs = 1):
         # it to compute the minimum enclosing circle and
         # centroid
         for obj in range(num_objs):
-            # c = max(cnts, key=cv2.contourArea)
-            # cnts.remove(c)
-            max_area = 0;
-            max_indx = 0;
-            for row in range(len(cnts)):
-                if cv2.contourArea(cnts[row]) > max_area:
-                    max_area = cv2.contourArea(cnts[row])
-                    max_indx = row
-            c = cnts[max_indx]
-            cnts.pop(max_indx)
+            c = max(cnts, key=cv2.contourArea)
+
             ((x, y), radius) = cv2.minEnclosingCircle(c)
             M = cv2.moments(c)
             center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
@@ -69,11 +68,22 @@ args = vars(ap.parse_args())
 
 # define the lower and upper boundaries of the "green"
 # ball in the HSV color space
+#TODO I am not convinced that these bounds are correct for HSV bc,
+#TODO they look much more appropriate for RGB
 greenLower = (29, 86, 6)
-greenUpper = (64, 255, 255)
+greenUpper = (90, 255, 255)
+
+redLower = (86, 6, 29)
+redUpper = (255, 64, 255)
+
+
 # initialize the list of tracked points, the frame counter,
 # and the coordinate deltas
-pts = deque(maxlen=args["buffer"])
+pts1 = deque(maxlen=args["buffer"])
+pts2 = deque(maxlen=args["buffer"])
+pts_list = []
+pts_list.append(pts1)
+pts_list.append(pts2)
 counter = 0
 (dX, dY) = (0, 0)
 direction = ""
@@ -106,44 +116,60 @@ while True:
     # resize the frame, blur it, and convert it to the HSV
     # color space
     frame = imutils.resize(frame, width=600)
-    pts = detect_objects(frame, pts, num_objs)
 
+    pts_list[0] = detect_objects(frame, pts_list[0], num_objs)
+    if (num_objs >1):
+        pts_list[1] = detect_objects(frame, pts_list[1], num_objs, 'red')
+        #pts for the SECOND ball
+
+    #TODO place the below code into a loop (for p in range(2)) and repeat this for both balls
     # loop over the set of tracked points
-    for i in np.arange(1, len(pts)):
-        # if either of the tracked points are None, ignore
-        # them
-        if pts[i - 1] is None or pts[i] is None:
-            continue
-        # check to see if enough points have been accumulated in
-        # the buffer
-        if counter >= 10 and i == 1 and len(pts) > 10:
-            # compute the difference between the x and y
-            # coordinates and re-initialize the direction
-            # text variables
-            dX = pts[-10][0] - pts[i][0]
-            dY = pts[-10][1] - pts[i][1]
-            (dirX, dirY) = ("", "")
-            direction= "Test"
+    for p in range(num_objs):
+        pts = pts_list[p]
+        for i in np.arange(1, len(pts)):
+            # if either of the tracked points are None, ignore
+            # them
+            if pts[i - 1] is None or pts[i] is None:
+                continue
+            # check to see if enough points have been accumulated in
+            # the buffer
+            if counter >= 10 and i == 1 and len(pts) > 10:
+                # compute the difference between the x and y
+                # coordinates and re-initialize the direction
+                # text variables
+                dX = pts[-10][0] - pts[i][0]
+                dY = pts[-10][1] - pts[i][1]
+                (dirX, dirY) = ("", "")
+                #TODO change this?
+                direction = "Test"
 
-            velocity = np.array([dX, dY])
-            point = pts[i]
-            if playing == False:
-                t = threading.Thread(target=play, args=(point, velocity, regions, references, playing))
-                t.start()
-                #play(point, velocity, regions, references, playing)
+                velocity = np.array([dX, dY])
+                point = pts[i]
+                if playing == False:
+                    t = threading.Thread(target=play, args=(point, velocity, regions, references, playing))
+                    t.start()
+                    # play(point, velocity, regions, references, playing)
 
-        # otherwise, compute the thickness of the line and
-        # draw the connecting lines
-        thickness = int(np.sqrt(args["buffer"] / float(i + 1)) * 2.5)
-        cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
-    # show the movement deltas and the direction of movement on
-    # the frame
-    cv2.putText(frame, direction, (10, 30), cv2.FONT_HERSHEY_SIMPLEX,
-        0.65, (0, 0, 255), 3)
-    cv2.putText(frame, "dx: {}, dy: {}".format(dX, dY),
-        (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX,
-        0.35, (0, 0, 255), 1)
+            # otherwise, compute the thickness of the line and
+            # draw the connecting lines
+            thickness = int(np.sqrt(args["buffer"] / float(i + 1)) * 2.5)
+            cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
+        # show the movement deltas and the direction of movement on
+        # the frame
+        cv2.putText(frame, direction, (10, 30), cv2.FONT_HERSHEY_SIMPLEX,
+                    0.65, (0, 0, 255), 3)
+        if p == 0:
+            cv2.putText(frame, "green - dx: {}, dy: {}".format(dX, dY),
+                    (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX,
+                    0.35, (0, 0, 255), 1)
+        else:
+            cv2.putText(frame, "red - dx: {}, dy: {}".format(dX, dY),
+                        (frame.shape[1] - 150, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX,
+                        0.35, (0, 0, 255), 1)
+
     # show the frame to our screen and increment the frame counter
+    #this next line let's you see what a frame in HSV looks like. debugging purposes only:
+    #frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     cv2.imshow("Frame", frame)
     key = cv2.waitKey(1) & 0xFF
     counter += 1
